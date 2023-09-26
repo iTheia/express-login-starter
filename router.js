fa44a7661757b48c44b6d820fb0c1f99e7f8cd3b
Sirakusa <herrera.clip@gmail.com>
2023-09-25 23:56:32 -0600

    create login and register routes

--- router.js
+++ router.js
@@ -1,4 +1,15 @@
 module.exports = function (app) {
+
+  app.post('/login',(req,res) => {
+    const userLogin = req.body
+    res.sendStatus(200)
+  })
+
+  app.post('/register',(req,res) => {
+    const userRegister = req.body
+    res.sendStatus(200)
+  })
+
   app.use((req, res) => {
     res.status(404).send('This route it is not being supported yet!');
   });
