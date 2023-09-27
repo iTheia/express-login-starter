9880c68a5d84c811651e2eedae0ae48d250ffe1c
Jose Luis Herrera <107272842+Sirakusa@users.noreply.github.com>
2023-09-26 22:54:43 -0600

    Update controller.js
    add class controller

--- modules/auth/controller.js
+++ modules/auth/controller.js
@@ -1,41 +1,4 @@
 
 class Controller {
 
-    constructor(json) {
-        this._json = json
-    }
-
-    verifyJson() {
-        _sameFormat()
-
-    }
-
-    _sameFormat() {
-        const formatJson = {
-            "user": "",
-            "password": ""
-        }
-
-        const moldJson = Object.keys(formatJson)
-        const userJson = Object.keys(this._json)
-
-        if (JSON.stringify(userJson) !== JSON.stringify(moldJson)) {
-            return false
-        }
-
-        return true
-    }
-
-    especialCharacters(){
-        
-    }
 }
-
-const userJson = {
-    "user": "gio",
-    "password": "1432"
-}
-
-const json = new Controller(userJson)
-
-console.log(json.verifyJson())
