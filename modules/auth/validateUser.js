3821e55125f6feedd7118c6e4e39a9f8ac86830d
Sirakusa <herrera.clip@gmail.com>
2023-09-26 23:04:28 -0600

    add class user and password

---
+++ modules/auth/validateUser.js
@@ -0,0 +1,34 @@
+const Joi = require('joi');
+
+
+
+class validateUser {
+
+    constructor(NameUser, Joi) {
+        this.NameUser = NameUser
+        this.Joi = Joi
+        this.userSchema
+    }
+
+    _configuration() {
+        this.userSchema = this.Joi.string()
+            .alphanum()
+            .min(3)
+            .max(30)
+            .required()
+    }
+
+    validate() {
+
+        this._configuration()
+
+        const { error, value } = this.userSchema.validate(this.NameUser)
+
+
+        if (error) {
+            return (`user validation error: ${error.details[0].message}`)
+        } else {
+            return (`valid user: ${value}`)
+        }
+    }
+}
