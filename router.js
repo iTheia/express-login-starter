32e370d845b75b7667b38041ea25e56162d9175a
Marlon <martinez.ded@gmail.com>
2023-09-26 20:59:01 -0600

    ejemplo

--- router.js
+++ router.js
@@ -1,4 +1,11 @@
 module.exports = function (app) {
+
+  app.get("/register", (req, res) =>{
+    const data = req.body
+    
+    res.send(result)
+  })
+
   app.use((req, res) => {
     res.status(404).send('This route it is not being supported yet!');
   });
