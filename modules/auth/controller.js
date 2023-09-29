d08932328ac365bc8afb8bfc57de5ddb2c1a5e2a
Sirakusa <herrera.clip@gmail.com>
2023-09-29 00:58:59 -0600

    response token

--- modules/auth/controller.js
+++ modules/auth/controller.js
@@ -3,15 +3,18 @@ const { hash } = require('bcrypt');
 const { encryption, existPassword } = require('./bcryptUtils.js')
 const { validateRegister, userExist } = require('./validation.js')
 const { usersDatabase } = require('./repository.js')
+const jwt = require('jsonwebtoken');
 
 async function postLogin(req, res) {
+
     try {
 
         const validatedPayload = await validateRegister({ user: req.body.user });
 
-        const match = await existPassword(req.body.password, req.body.user, usersDatabase)
+        const match = await existPassword(req.body.password, req.body.user, usersDatabase);
 
-        if(!match){
+
+        if (!match) {
             return res.send("Password is not correct")
         }
 
@@ -19,7 +22,10 @@ async function postLogin(req, res) {
         return res.status(400).send(error);
     }
 
-    return res.sendStatus(200)
+    var jwt = require('jsonwebtoken');
+    var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+
+    return res.send(token)
 }
 
 async function postRegister(req, res) {
